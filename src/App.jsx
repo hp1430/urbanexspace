08bb833f29c233e1a438ef0f5f5e28312ceba33d
Himanshu Parashar <parasharhimanshu41@gmail.com>
2025-04-29 15:52:25 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="text-center mt-10 text-3xl font-bold text-blue-600">
+      Tailwind is working! 🎉
+    </div>
+  )
+}
+
+export default App
