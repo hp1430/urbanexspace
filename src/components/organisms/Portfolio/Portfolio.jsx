b477522e17aff13b2ac30b5426368e49b7a5d1ca
Himanshu Parashar <parasharhimanshu41@gmail.com>
2025-07-31 20:03:52 +0530

    added portfolio section

---
+++ src/components/organisms/Portfolio/Portfolio.jsx
@@ -0,0 +1,88 @@
+import { Parallax } from "../../atoms/Parallax/Parallax"
+import Office from "../../../assets/categories/OfficeCover.png"
+import Residential from "../../../assets/categories/ResidentialCover.png"
+import Hospitality from "../../../assets/categories/HospitalityCover.png"
+import FarmhouseCover from "../../../assets/categories/FarmhouseCover.png"
+import { useLocation } from "react-router-dom"
+import { useEffect } from "react"
+
+
+export const Portfolio = () => {
+
+    const location = useLocation();
+
+    useEffect(() => {
+        if(location.hash === "#portfolio") {
+            const id = location.hash.substring(1);
+            const element = document.getElementById(id);
+            if(element) {
+                element.scrollIntoView({ behavior: "smooth"} );
+            }
+        }
+    }, [location])
+    return (
+        <div id="portfolio">
+            <h1
+                className="text-5xl font-semibold text-center mt-20 mb-10 text-gray-800"
+            >
+                Portfolio
+            </h1>
+
+            <Parallax image={Residential} description={'Residential Designs'} />
+            <div className="w-full mt-20 mb-10 px-4 flex justify-center">
+                <div className="w-full max-w-2xl text-center">
+                    <h2 className="text-2xl md:text-3xl font-semibold text-black mb-6">
+                    Designed for Modern Living, Styled for Timeless Grace.
+                    </h2>
+                    <p className="text-base md:text-lg lg:text-xl font-light text-black leading-relaxed">
+                    Our Residential Designs are tailored to urban lifestyles that demand both style and substance.
+                    Each project is a study in balance — blending bold design statements with serene palettes,
+                    smart space planning with luxurious comfort. We focus on making every corner count, from 
+                    multi-functional layouts to mood-enhancing lighting and curated finishes.
+                    </p>
+                </div>
+            </div>
+
+            <Parallax image={Office} description={'Office Designs'} />
+            <div className="w-full mt-20 mb-10 px-4 flex justify-center">
+                <div className="w-full max-w-2xl text-center">
+                    <h2 className="text-2xl md:text-3xl font-semibold text-black mb-6">
+                    Smart & Stylish Workspaces That Energize Performance.
+                    </h2>
+                    <p className="text-base md:text-lg lg:text-xl font-light text-black leading-relaxed">
+                    We believe a well-designed office is a catalyst for productivity and inspiration. Our approach to office interiors combines brand identity with employee experience using layout, lighting, color psychology, and material choices to create dynamic environments.
+                    From sleek corporate setups to creative studios, our designs support modern workflows while exuding professionalism and innovation.
+                    </p>
+                </div>
+            </div>
+
+            <Parallax image={Hospitality} description={'Hospitality Designs'} />
+            <div className="w-full mt-20 mb-10 px-4 flex justify-center">
+                <div className="w-full max-w-2xl text-center">
+                    <h2 className="text-2xl md:text-3xl font-semibold text-black mb-6">
+                    Immersive Luxury Designed to Leave a Lasting Impression.
+                    </h2>
+                    <p className="text-base md:text-lg lg:text-xl font-light text-black leading-relaxed">
+                    Our hospitality interiors blend elegance, warmth, and cultural nuance to create unforgettable guest experiences.
+                    Whether designing boutique hotels or expansive resorts, we focus on spatial storytelling where every lobby, room,
+                    and lounge evokes comfort, opulence, and distinct identity. Rich materials, ambient lighting, and regionally
+                    inspired accents come together to build immersive environments that delight guests and elevate your brand’s
+                    hospitality offering.
+                    </p>
+                </div>
+            </div>
+
+            <Parallax image={FarmhouseCover} description={'Farmhouse Designs'} />
+            <div className="w-full mt-20 mb-10 px-4 flex justify-center">
+                <div className="w-full max-w-2xl text-center">
+                    <h2 className="text-2xl md:text-3xl font-semibold text-black mb-6">
+                    Where Nature Embraces Luxury, Effortlessly.
+                    </h2>
+                    <p className="text-base md:text-lg lg:text-xl font-light text-black leading-relaxed">
+                        Our farmhouse designs are rooted in simplicity, grounded in nature, and elevated by elegance. We craft interiors that breathe with airy layouts, raw materials, and cozy textures that reflect the surrounding landscape. Whether it's a weekend escape or a full-time retreat, our farmhouse spaces offer a perfect blend of tranquility and sophistication, where comfort feels organic and style feels effortless.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
