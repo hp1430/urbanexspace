6d6e3e806a4299be8a026aa030695e460877fcbd
DEEPAK KUMAR <rohit31922r@gmail.com>
2025-08-01 22:39:55 +0530

    hospitalitySlide images uploaded

--- src/pages/HospitalityPortfolio/HospitalityPortfolio.jsx
+++ src/pages/HospitalityPortfolio/HospitalityPortfolio.jsx
@@ -1,9 +1,21 @@
 import { Navbar } from "../../components/atoms/Navbar/Navbar"
 import { PortfolioPage } from "../../components/organisms/PortfolioPage/PortfolioPage"
-import residential11 from "../../assets/portfolio/residential/residential11.png"
-import residential12 from "../../assets/portfolio/residential/residential12.png"
-import residential21 from "../../assets/portfolio/residential/residential21.png"
-import residential22 from "../../assets/portfolio/residential/residential22.png"
+import hospitality11 from "../../assets/portfolio/hospitality/hospitality11.png"
+import hospitality12 from "../../assets/portfolio/hospitality/hospitality12.png"
+import hospitality21 from "../../assets/portfolio/hospitality/hospitality21.png"
+import hospitality22 from "../../assets/portfolio/hospitality/hospitality22.png"
+import HospitalitySlide11 from "../../assets/portfolio/hospitality/HospitalitySlide11.png"
+import HospitalitySlide12 from "../../assets/portfolio/hospitality/HospitalitySlide12.png"
+import HospitalitySlide13 from "../../assets/portfolio/hospitality/HospitalitySlide13.png"
+import HospitalitySlide14 from "../../assets/portfolio/hospitality/HospitalitySlide14.png"
+import HospitalitySlide15 from "../../assets/portfolio/hospitality/HospitalitySlide15.png"
+import HospitalitySlide21 from "../../assets/portfolio/hospitality/HospitalitySlide21.png"
+import HospitalitySlide22 from "../../assets/portfolio/hospitality/HospitalitySlide22.png"
+import HospitalitySlide23 from "../../assets/portfolio/hospitality/HospitalitySlide23.png"
+import HospitalitySlide24 from "../../assets/portfolio/hospitality/HospitalitySlide24.png"
+import HospitalitySlide25 from "../../assets/portfolio/hospitality/HospitalitySlide25.png"
+import HospitalitySlide26 from "../../assets/portfolio/hospitality/HospitalitySlide26.png"
+import HospitalitySlide27 from "../../assets/portfolio/hospitality/HospitalitySlide27.png"
 
 export const HospitalityPortfolio = () => {
     return (
@@ -23,16 +35,24 @@ export const HospitalityPortfolio = () => {
 
                 <PortfolioPage
                     cards={[
-                        { image: residential11 },
-                        { image: residential12 },
-                        { image: residential21 },
-                        { image: residential22 },
+                        { image: hospitality11 },
+                        { image: hospitality12 },
+                        { image: hospitality21 },
+                        { image: hospitality22 },
                     ]}
                     images={[
-                        { image: residential11 },
-                        { image: residential12 },
-                        { image: residential21 },
-                        { image: residential22 },
+                        { image: HospitalitySlide11 },
+                        { image: HospitalitySlide12 },
+                        { image: HospitalitySlide13 },
+                        { image: HospitalitySlide14 },
+                        { image: HospitalitySlide15 },
+                        { image: HospitalitySlide21 },
+                        { image: HospitalitySlide22 },
+                        { image: HospitalitySlide23 },
+                        { image: HospitalitySlide24 },
+                        { image: HospitalitySlide25 },
+                        { image: HospitalitySlide26 },
+                        { image: HospitalitySlide27 },
                     ]}
                 />
             </div>
